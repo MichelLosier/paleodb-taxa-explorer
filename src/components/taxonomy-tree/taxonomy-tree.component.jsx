44923fc0e7ea7c8427017f1dec6e337c3ff2097d
Michel Losier <mlosier@misha-ubuntu18.04>
2018-11-17 16:25:43 -0800

    init branch

---
+++ src/components/taxonomy-tree/taxonomy-tree.component.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+class TaxonomyTree extends React.Component {
+    constructor(){
+        super();
+        this.state = {
+            graph: {}
+        }
+    }
+
+    render(){
+        return(
+            <div className="taxonomy-tree-container">
+
+            </div>
+        )
+    }
+}
+
+export default TaxonomyTree;
